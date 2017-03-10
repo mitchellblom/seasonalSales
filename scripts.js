9a71c4d2e1f13604fc575986be95697ac17203c0
Mitchell Blom <mitchellblom@gmail.com>
2017-03-10 13:40:13 -0600

    all products writing by dept to dom

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 /////////////// REFERENCES /////////////////////
 
 var output = document.getElementById("output");
-var myRequest = new XMLHttpRequest();
+var deptRequest = new XMLHttpRequest();
+var prodRequest = new XMLHttpRequest();
 
 
-
-////////////// EXECUTIONS //////////////////////
+////////////// DEPT DATA TO DOM //////////////////////
 
 function categoriesDom(xhrData) { //xhrData comes from makeDom(data) in the executeCategories function
     var categoryString = "";
@@ -15,16 +15,57 @@ function categoriesDom(xhrData) { //xhrData comes from makeDom(data) in the exec
         categories = xhrData.categories[i];
         categoryString += `<div class="departments">`;
         categoryString += `<div>${categories.name}</div>`;
-        categoryString += `<div>${categories.season_discount}</div>`;
+        categoryString += `<div id="${categories.name}"></div>`;
+        categoryString += `<div id="${categories.season_discount}"></div>`;
         categoryString += `<div>${categories.discount}</div>`;
         categoryString += `</div>`;
     }
 		output.innerHTML = categoryString;
 }
 
+////////////// PRODUCT DATA TO DOM /////////////////
+
+function productDomSwitch(data) {
+	var apparel = document.getElementById("Apparel");
+	var furniture = document.getElementById("Furniture");
+	var apparel = document.getElementById("Household");
+
+	var appString = "";
+	var furnString = "";
+	var houseString = "";
+
+	for (var i = 0; i < data.products.length; i++){
+			var prodInfo = data.products[i];	
+			// console.log(prodInfo);
+
+		switch (prodInfo.category_id){
+	        // Apparel
+		        case 1:
+		            // console.log("Apparel: ", prodInfo.name);
+		        break;
+	        // Furniture
+		        case 2: 
+		            // console.log("Furniture: ", prodInfo.name);
+		        break;
+		    // Household
+		        case 3: 
+		            // console.log("Household: ", prodInfo.name);
+		        break;
+	        }
+	    }
+	};
+
+
+////////////// EXECUTIONS //////////////////////////
+
 function executeCategories() {
     var data = JSON.parse(this.responseText);
     categoriesDom(data);
+}
+
+function executeProducts() {
+    var data = JSON.parse(this.responseText);
+    productDomSwitch(data);
 }
 
 function executeFailure() {
@@ -33,7 +74,13 @@ function executeFailure() {
 
 ///////////// REQUESTS /////////////////////////
 
-myRequest.addEventListener("load", executeCategories);
-myRequest.addEventListener("error", executeFailure);
-myRequest.open("GET", "categories.json");
-myRequest.send();
+deptRequest.addEventListener("load", executeCategories);
+deptRequest.addEventListener("error", executeFailure);
+deptRequest.open("GET", "categories.json");
+deptRequest.send();
+
+prodRequest.addEventListener("load", executeProducts);
+prodRequest.addEventListener("error", executeFailure);
+prodRequest.open("GET", "products.json");
+prodRequest.send();
+
