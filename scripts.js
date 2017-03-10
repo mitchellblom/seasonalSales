474528bd0e81aef9cd92191043c5e8196efa5e68
Mitchell Blom <mitchellblom@gmail.com>
2017-03-10 09:55:54 -0600

    one category printing to dom

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
-// console.log("scripts work");
+/////////////// REFERENCES /////////////////////
+
+var output = document.getElementById("output");
+var myRequest = new XMLHttpRequest();
+
+
+
+////////////// EXECUTIONS //////////////////////
+
+function categoriesDom(xhrData) { //xhrData comes from makeDom(data) in the executeCategories function
+    var catString = "";
+    var categories = "";
+
+    for (var i = 0; i < xhrData.categories.length; i++) {
+        console.log("dept loop works");
+        categories = xhrData.categories[i];
+    }
+        catString += `<div>${categories.name}</div>`;
+		output.innerHTML = catString;
+}
+
+function executeCategories() {
+    var data = JSON.parse(this.responseText);
+    categoriesDom(data);
+}
+
+function executeFailure() {
+    output.innerHTML = "Looks like the store site is down. Please check back later.";
+}
+
+///////////// REQUESTS /////////////////////////
+
+myRequest.addEventListener("load", executeCategories);
+myRequest.addEventListener("error", executeFailure);
+myRequest.open("GET", "categories.json");
+myRequest.send();
